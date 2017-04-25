be2eef5ee4658b9325ff6fafc957d6de2b0fbf08
superhan16 <hannah.premo@gmail.com>
2017-04-25 15:53:33 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import testy
+
+test_dino = dino.Dino
+
+test_calc_grav = testy.calc_grave()
+print(test_calc_grav.getCoordinates())
+#this tests the gravity function
+test_jump = testy.jump()
+print(test_jump.getCoordinates())
+#this tests the jump function
+test_fall = testy.fall()
+print(test_fall.getCoordinates())
+#this tests the fall function
+
