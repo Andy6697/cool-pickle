2520cf9e05366dfd52c88e7b41e9b15ead8ef150
Andy6697 <azou4@binghamton.edu>
2017-04-29 15:20:42 -0400

    Delete cactus.py

--- cactus.py
+++
@@ -1,6 +0,0 @@
-class Cactus(pygame.sprite.Sprite):
-    def __init__ (self,color,width,height):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width,height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
