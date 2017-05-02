e6e878db503b423114cdb307ba07523513eb3cb5
Andy6697 <azou4@binghamton.edu>
2017-05-02 15:17:14 -0400

    Create model.py
    This is the model file with the classes. It is imported from controller.py

---
+++ model.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__ (self,width,height, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        #self.image = pygame.Surface([width,height])
+        #self.image.fill(color)
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("cactus.png").convert()
+        self.rect = pygame.Rect(self.x, self.y, 117, 160)
+
+        #self.image.set_colorkey(white)
+        #screen.blit(self.image, [x, y])
+        #pygame.display.flip()
+
+        #self.rect = self.image.get_rect()
+class Dino(pygame.sprite.Sprite):
+    def  __init__(self, width, height, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        #self.image = pygame.Surface([width,height])
+        #self.image.fill(color)
+
+        self.image = pygame.image.load("Hankk_the_dino.png").convert()
+        self.x = x
+        self.y = y
+#self.rect determines the location stored for the computer to know where you are
+        self.rect = pygame.Rect(self.x, self.y, 80, 84)
+        self.height = 0
+        self.reach = True
+
+        #self.image.set_colorkey(white)
+        #screen.blit(self.image, [x, y])
+
+    def jump(self):
+        if not self.reach:
+            if self.height != 250:
+                self.rect.y -= 25
+                self.height += 25
+            else: self.reach = True
+    def fall(self):
+        if self.reach:
+            if self.height != 0:
+                self.rect.y += 25
+                self.height -= 25
